5d7a61680db725a0cce23b42bd9a003e1146b911
Noor Mohammed <nooru.mohad@gmail.com>
2021-02-20 15:30:37 +0530

    Counter

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
 class Todo extends Component {
+   state = {
+      clickCount: 0,
+   };
+
+   clickEventInButton = () => {
+      this.setState({
+         clickCount: this.state.clickCount + 1,
+      });
+   };
    render() {
       return (
          <div className="ele">
             <h4>Hello from todo component</h4>
             <p className="props">{this.props.dataToComponents}</p>
-            <p>after props</p>
+            <button onClick={this.clickEventInButton}>Click Me</button>
+
+            <p> {this.state.clickCount}</p>
          </div>
       );
    }
