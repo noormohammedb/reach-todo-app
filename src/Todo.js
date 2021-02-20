5725a8d649df892d0b4d6fd29eca472e000b8aa1
Noor Mohammed <nooru.mohad@gmail.com>
2021-02-20 22:13:41 +0530

    todo add  and remove

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
 
 class Todo extends Component {
+   state = {
+      todoData: [],
+   };
+
+   removeEvent = (possition) => {
+      console.log("event clicked");
+      console.log(possition);
+      this.setState({
+         todoData: this.state.todoData.filter(
+            (data, index) => index !== possition
+         ),
+      });
+   };
+
+   addEvent = (event) => {
+      event.preventDefault();
+      console.log(event.target.previousSibling.value);
+      this.setState({
+         todoData: [...this.state.todoData, event.target.previousSibling.value],
+      });
+      event.target.previousSibling.value = "";
+   };
+
+   lookArray = (event) => {
+      console.log(this.state);
+   };
+
    render() {
       return (
          <React.Fragment>
@@ -8,11 +35,13 @@ class Todo extends Component {
             <div>
                <form>
                   <input type="text" name="todo-box" id="todo-box" />
-                  <button>Add</button>
+                  <button type="submit" onClick={this.addEvent}>
+                     Add
+                  </button>
                </form>
                <div className="todo-containe">
                   <ul className="todo-list">
-                     <li className="ch-to">
+                     {/* <li className="ch-to">
                         <p className="todo-content">{}blabal</p>
                         <img
                            src="https://www.flaticon.com/premium-icon/icons/svg/2920/2920658.svg"
@@ -27,9 +56,23 @@ class Todo extends Component {
                            alt=""
                            className="cross-icon"
                         />
-                     </li>
+                     </li> */}
+                     {this.state.todoData.map((data, index) => (
+                        <li key={index} className="ch-to">
+                           <p className="todo-content">{data}</p>
+                           <img
+                              src="https://www.flaticon.com/premium-icon/icons/svg/2920/2920658.svg"
+                              alt=""
+                              className="cross-icon"
+                              onClick={() => {
+                                 this.removeEvent(index);
+                              }}
+                           />
+                        </li>
+                     ))}
                   </ul>
                </div>
+               <button onClick={this.lookArray}>check state</button>
             </div>
          </React.Fragment>
       );
