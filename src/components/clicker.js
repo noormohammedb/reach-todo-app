07981832d29b8ef62ee8c5d8230fddee00555298
Noor Mohammed <nooru.mohad@gmail.com>
2021-02-20 15:52:48 +0530

    props to state

--- src/components/clicker.js
+++ src/components/clicker.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
 class Clicker extends Component {
+   state = {
+      changeProps: this.props.count,
+   };
+
+   changeProsFun = () => {
+      this.setState({
+         changeProps: this.props.count + 1,
+      });
+   };
+
    render() {
       return (
          <div>
             <p>Hello form clickger</p>
             <p className="count">{this.props.count}</p>
+            <p className="clolor">{this.state.changeProps}</p>
+            <button className="propsfun" onClick={this.changeProsFun}>
+               ch props
+            </button>
          </div>
       );
    }
